d3182a74709c4b42dc4a7d0d616aebfd2569fd0a
phuocthuan123vt <24521739@gm.uit.edu.vn>
2025-12-13 16:46:52 +0700

    Interaction system basic done

---
+++ Assets/_Game/Scripts/Items/TrashBin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TrashBin : MonoBehaviour, IInteractable // Kế thừa từ Interface vừa tạo
+{
+    public void Interact()
+    {
+        // Logic khi bấm E vào thùng rác
+        Debug.Log("Lục lọi thùng rác... Nhặt được 1 cái chai nhựa!");
+
+        // (Tạm thời) Đổi màu để biết là đã tương tác
+        GetComponent<SpriteRenderer>().color = Color.gray;
+
+        // Sau này sẽ code thêm: +1 Chai nhựa vào túi đồ
+    }
+}
---
+++ Assets/_Game/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float _interactRange = 1.5f; // Bán kính quét
+    [SerializeField] private LayerMask _interactLayer;   // Chỉ quét những thứ thuộc lớp này
+
+    private void Update()
+    {
+        // Nếu bấm nút E
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CheckInteraction();
+        }
+    }
+
+    private void CheckInteraction()
+    {
+        // Tạo 1 vòng tròn vô hình tại vị trí Player
+        Collider2D[] hitObjects = Physics2D.OverlapCircleAll(transform.position, _interactRange, _interactLayer);
+
+        foreach (Collider2D obj in hitObjects)
+        {
+            // Kiểm tra xem vật đó có script chứa IInteractable không
+            IInteractable interactable = obj.GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                interactable.Interact();
+                return; // Chỉ tương tác với 1 cái gần nhất rồi thôi
+            }
+        }
+    }
+
+    // Vẽ cái vòng tròn ra màn hình Scene để dễ chỉnh (Debug)
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _interactRange);
+    }
+}
